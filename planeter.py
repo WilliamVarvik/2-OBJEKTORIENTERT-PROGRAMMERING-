5e27d69f6b0c926e9ad8b254778c7c93c56c6b5a
WilliamVarvik <william.varvik@gmail.com>
2023-10-30 10:56:35 +0100

    .
    .

--- planeter.py
+++ planeter.py
@@ -10,16 +10,20 @@ class Planet:
 
 jorden = Planet( "Jorden", 152, 6371)
 
+def visInfo(self):
+    print(f"Planeten {self.navn} er {self.solavstand} millioner km unna sola og har radius {self.radius} km.")
 
+jorden.visInfo()
+print(jorden.navn)
+print(jorden.antallRinger)
 # Oppgave 1: 
 # Lag et objekt for Mars, Jupiter og Jorda, der du lagrer informasjon om navn, solavstand og radius. Lagre disse objektene i egne variabler
 
 # Hva skjer om du skriver print(Jorda)?
 # Hva skjer om du skriver print(Jorda.navn)?
 # Prøv å skriv ut de andre attributtene til klassen
-print(jorden)
-print(jorden.navn)
-print(jorden.antallRinger)
+
+
 # OBS: pass på rekkefølgen i argumentene til konstruktøren.
 
 
--- planeter_med_dokumentering.py
+++ planeter_med_dokumentering.py
@@ -1,6 +1,6 @@
 
-class Planet:
-    """ En klasse for å lage planet-objekter 
+# class Planet:
+""" En klasse for å lage planet-objekter 
 
     Parametre:
         navn (str): Planetens navn
@@ -10,17 +10,36 @@ class Planet:
  """
 
 
-    def __init__(self,navn:str,solavstand:float,radius:float, antallRinger=0) -> None:
+#     def __init__(self,navn:str,solavstand:float,radius:float, antallRinger=0) -> None:
 
-        self.navn=navn
-        self.solavstand=solavstand
-        self.radius = radius
-        self.antallRinger = antallRinger
+#         self.navn=navn
+#         self.solavstand=solavstand
+#         self.radius = radius
+#         self.antallRinger = antallRinger
 
-jorden = Planet( "Jorden", 152, 6371)
+# jorden = Planet( "Jorden", 152, 6371)
 
 # Oppgave 1: 
 # Lag et objekt for Mars, Jupiter og Jorda, der du lagrer informasjon om navn, solavstand og radius. Lagre disse objektene i egne variabler
+
+class Planeter:
+    def __init__(self,navn:str,solavstand:float,radius:float, antallringer=0) -> None:
+
+        self.navn = navn
+        self.solavstand = solavstand
+        self.radius = radius
+        self.antallringer = antallringer
+
+jorden = Planeter("Jorden", 152, 6371)
+mars = Planeter("Mars",227.9, 3389.5)
+jupiter = Planeter("Jupiter", 778, 69911,1)
+
+print(jorden)
+print(jorden.navn)
+def visInfo(self):
+    print(f"Planeten {self.navn} er {self.solavstand} millioner km unna sola og har radius {self.radius} km.")
+
+jorden.visInfo()
 
 # Hva skjer om du skriver print(Jorda)?
 # Hva skjer om du skriver print(Jorda.navn)?
